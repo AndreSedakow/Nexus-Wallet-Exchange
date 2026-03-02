5dcf575c2f2ec25287761b6af8596cdb668a1239
Andre Luiz Sedakow Nascimento <andreluizsedakow@gmail.com>
2026-03-02 20:14:29 -0300

    chore: bootstrap backend structure with postgres and prisma

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Nexus Crypto API Running" }));
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
+ 
